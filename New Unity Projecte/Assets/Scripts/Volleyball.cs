863d7d150c7b09956bf8c0d80f2a740d30b3d4bc
Josh <josh.church.josh@gmail.com>
2022-08-30 16:12:08 -0400

    Max speed on rigidbody is now enforced

--- New Unity Projecte/Assets/Scripts/Volleyball.cs
+++ New Unity Projecte/Assets/Scripts/Volleyball.cs
@@ -1,11 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Volleyball : MonoBehaviour
 {
     [SerializeField] private float _hitStrength = 300f;
+
+    [SerializeField] private float _maxSpeed = 10f;
+
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField] private float _currMagnitude;
+    
+    [SerializeField] private Vector2 _currVelocity;
+
+    private void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.velocity.magnitude > _maxSpeed)
+        {
+            // get the percentage that the current magnitude of velocity is over max speed
+            float percentageOver = _maxSpeed / _rigidbody.velocity.magnitude;
+            Debug.Log("Velocity before multiplication: " + _rigidbody.velocity);
+            // multiply the velocity by percentage over so the velocity is _maxSpeed
+            _rigidbody.velocity *= percentageOver;
+            Debug.Log("Velocity after multiplication: " + _rigidbody.velocity);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Projectile"))
